d4c2f11810af5bcfbb248447789d949e866ef635
Leonardo-LRD <leonardo.vasconcellos@sptech.school>
2022-04-23 22:20:43 -0300

    Script atualizado

--- Banco de Dados/v4/Script/Script.sql
+++ Banco de Dados/v4/Script/Script.sql
@@ -40,18 +40,24 @@ CREATE TABLE Estacionamento (
 	FOREIGN KEY (fkEmpresa) REFERENCES Empresa (idEmpresa)
 );
 
-+------------------+--------------+------+-----+---------+----------------+
-| Field            | Type         | Null | Key | Default | Extra          |
-+------------------+--------------+------+-----+---------+----------------+
-| idEstacionamento | int          | NO   | PRI | NULL    | auto_increment |
-| cep              | varchar(20)  | YES  |     | NULL    |                |
-| Bairro           | varchar(60)  | YES  |     | NULL    |                |
-| uf               | varchar(2)   | YES  |     | NULL    |                |
-| numVagas         | int          | YES  |     | NULL    |                |
-| fkEmpresa        | int          | YES  | MUL | NULL    |                |
-+------------------+--------------+------+-----+---------+----------------+;
+//
 
--- Tabela Administrador [relação n:1 com Empresa]:
+INSERT INTO Estacionamento (cep, numero, uf, numVagas, fkEmpresa) VALUES 
+-- id1:
+(01310916, 1374, 'SP', 600, 1),
+-- id2:
+(01046001, 112, 'SP', 450, 1),
+-- id3:
+(01332000, 636, 'SP', 200, 2),
+-- id4:
+(01318000, 306, 'SP', 300, 2),
+-- id5:
+(01301000, 247, 'SP', 400, 3),
+-- id6:
+(01412000, 1808, 'SP', 250, 3);
+
+
+-- Tabela Usuario [relação n:1 com Empresa]:
 
 CREATE TABLE Usuario (
 	idUsuario INT PRIMARY KEY AUTO_INCREMENT,
@@ -77,7 +83,7 @@ CREATE TABLE Usuario (
 | fkAdmChefe      | int         | YES  | MUL | NULL    |                |
 +-----------------+-------------+------+-----+---------+----------------+
 
-INSERT INTO Administrador (nome, sobrenome, email, senha, fkEmpresa, fkAdmChefe) VALUES
+INSERT INTO Usuario (nome, sobrenome, email, senha, fkEmpresa, fkAdmChefe) VALUES
 -- id1: 
 ('Karen', 'Albuquerque', 'Karen@estapar.com', 'IEX22dbI', 1, null),
 -- id2: 
@@ -93,13 +99,13 @@ INSERT INTO Administrador (nome, sobrenome, email, senha, fkEmpresa, fkAdmChefe)
 
 
 
--- Tabela Administrador_Estacionamento [relação N:m Administrador_has_Estacionamento]:
+-- Tabela Usuario_Estacionamento [relação N:m Usuario_has_Estacionamento]:
 
 CREATE TABLE Usuario_Estacionamento (
 	fkUsuario INT,
 	fkEstacionamento INT,
-	PRIMARY KEY (fkAdministrador, fkEstacionamento),
-	FOREIGN KEY (fkAdministrador) REFERENCES Usuario (idUsuario),
+	PRIMARY KEY (fkUsuario, fkEstacionamento),
+	FOREIGN KEY (fkUsuario) REFERENCES Usuario (idUsuario),
 	FOREIGN KEY (fkEstacionamento) REFERENCES Estacionamento (idEstacionamento)
 );
 
@@ -110,7 +116,7 @@ CREATE TABLE Usuario_Estacionamento (
 | fkEstacionamento | int  | NO   | PRI | NULL    |       |
 +------------------+------+------+-----+---------+-------+
 
-INSERT INTO Administrador_Estacionamento (fkAdministrador, fkEstacionamento) VALUES 
+INSERT INTO Usuario_Estacionamento (fkUsuario, fkEstacionamento) VALUES 
 -- id1:
 (1, 1),
 -- id2:
@@ -186,7 +192,6 @@ CREATE TABLE Dados (
 | fkEmpresa        | int     | NO   | PRI | NULL    |                |
 +------------------+---------+------+-----+---------+----------------+
 
--- Quando a PK é composta podemos repetir elas no insert? E quando é FK?
 INSERT INTO Dados (data, hora, state, fkSensor, fkEstacionamento, fkEmpresa) VALUES 
 -- id:1
 -- [Entrada]: Sensor1, Estacionamento1, Empresa1
@@ -239,6 +244,11 @@ SELECT * FROM Empresa;
 |         2 | MultiPark   | 31985344000120 |
 |         3 | Stop & Park | 57410487000106 |
 +-----------+-------------+----------------+
+
+
+SELECT * FROM Estacionamento;
+
+//
 
 
 SELECT * FROM Usuario;
@@ -304,31 +314,16 @@ SELECT * FROM Dados;
 
 
 SELECT CONCAT(Usuario.nome, " ", Usuario.sobrenome) AS Usuario, Usuario.email AS Email, 
-Usuario.senha AS Senha, CONCAT(AdmChefe.nome," ", AdmChefe.sobrenome) AS "Administrador Chefe" 
+Usuario.senha AS Senha, CONCAT(AdmChefe.nome," ", AdmChefe.sobrenome) AS Administrador
 FROM Usuario JOIN Usuario as AdmChefe ON Usuario.fkAdmChefe = AdmChefe.idUsuario;
 
-+----------------+----------------------+----------+---------------------+
-| Usuario        | Email                | Senha    | Administrador Chefe |
-+----------------+----------------------+----------+---------------------+
-| Robert Pereira | Robert@estapar.com   | 5Switbxz | Karen Albuquerque   |
-| Monica Costa   | Monica@multipark.com | UKu7TF5d | Miguel Ferrari      |
-| Pedro Gomes    | Pedro@stoppark.com   | G6P1YDq1 | Alice Nakamura      |
-+----------------+----------------------+----------+---------------------+
+//
 
 
-SELECT Estacionamento.idEstacionamento AS "Nº Estacionamento", Estacionamento.cep AS CEP, Estacionamento.endereco AS Endereço, Empresa.nome AS Empresa, Empresa.cnpj AS CNPJ 
+//SELECT Estacionamento.idEstacionamento AS "Nº Estacionamento", Estacionamento.cep AS CEP, Estacionamento.endereco AS Endereço, Empresa.nome AS Empresa, Empresa.cnpj AS CNPJ 
 FROM Estacionamento JOIN Empresa ON fkEmpresa = idEmpresa;
 
-+-------------------+----------+---------------------------------------------------------------+-------------+----------------+
-| Nº Estacionamento | CEP      | Endereço                                                      | Empresa     | CNPJ           |
-+-------------------+----------+---------------------------------------------------------------+-------------+----------------+
-|                 1 | 01310916 | Av. Paulista, 1374 - Bela Vista, São Paulo - SP               | Estapar     | 72423437000149 |
-|                 2 | 01046001 | Av. São Luís, 112 - Republica, São Paulo - SP                 | Estapar     | 72423437000149 |
-|                 3 | 01332000 | R. Itapeva, 636 - Bela Vista, São Paulo - SP                  | MultiPark   | 31985344000120 |
-|                 4 | 01318000 | Av. Brigadeiro Luís Antônio, 306 - Bela Vista, São Paulo - SP | MultiPark   | 31985344000120 |
-|                 5 | 01301000 | R. da Consolação, 247 - Consolação, São Paulo - SP            | Stop & Park | 57410487000106 |
-|                 6 | 01412000 | R. Augusta, 1808 - Cerqueira César, São Paulo - SP            | Stop & Park | 57410487000106 |
-+-------------------+----------+---------------------------------------------------------------+-------------+----------------+
+//
 
 
 SELECT Sensor.idSensor AS "Nº Sensor", Dados.state AS "Entra = 1 /Saida = 0", Dados.data AS Data, Dados.hora AS Hora 
@@ -352,48 +347,18 @@ FROM Sensor JOIN Dados ON fkSensor = idSensor;
 +-----------+----------------------+------------+----------+
 
 
-SELECT Empresa.nome AS "Nome da Empresa", Empresa.cnpj AS CNPJ, Estacionamento.idEstacionamento AS "Nº Estacionamento", Estacionamento.endereco AS "Endereço", 
+//SELECT Empresa.nome AS "Nome da Empresa", Empresa.cnpj AS CNPJ, Estacionamento.idEstacionamento AS "Nº Estacionamento", Estacionamento.endereco AS "Endereço", 
 Sensor.idSensor AS "Nº Sensor", Dados.state AS "Entra = 1 /Saida = 0", Dados.data AS "Data", Dados.hora AS Hora  
 FROM Dados JOIN Empresa ON fkEmpresa = idEmpresa JOIN Estacionamento ON fkEstacionamento = idEstacionamento JOIN Sensor ON fkSensor = idSensor;  
 
-+-----------------+----------------+-------------------+---------------------------------------------------------------+-----------+----------------------+------------+----------+
-| Nome da Empresa | CNPJ           | Nº Estacionamento | Endereço                                                      | Nº Sensor | Entra = 1 /Saida = 0 | Data       | Hora     |
-+-----------------+----------------+-------------------+---------------------------------------------------------------+-----------+----------------------+------------+----------+
-| Estapar         | 72423437000149 |                 1 | Av. Paulista, 1374 - Bela Vista, São Paulo - SP               |         1 |                    1 | 2022-04-26 | 12:00:00 |
-| Estapar         | 72423437000149 |                 1 | Av. Paulista, 1374 - Bela Vista, São Paulo - SP               |         1 |                    0 | 2022-04-26 | 13:00:00 |
-| Estapar         | 72423437000149 |                 2 | Av. São Luís, 112 - Republica, São Paulo - SP                 |         2 |                    1 | 2022-04-26 | 12:00:00 |
-| Estapar         | 72423437000149 |                 2 | Av. São Luís, 112 - Republica, São Paulo - SP                 |         2 |                    0 | 2022-04-26 | 13:00:00 |
-| MultiPark       | 31985344000120 |                 3 | R. Itapeva, 636 - Bela Vista, São Paulo - SP                  |         3 |                    1 | 2022-04-26 | 12:00:00 |
-| MultiPark       | 31985344000120 |                 3 | R. Itapeva, 636 - Bela Vista, São Paulo - SP                  |         3 |                    0 | 2022-04-26 | 13:00:00 |
-| MultiPark       | 31985344000120 |                 4 | Av. Brigadeiro Luís Antônio, 306 - Bela Vista, São Paulo - SP |         4 |                    1 | 2022-04-26 | 12:00:00 |
-| MultiPark       | 31985344000120 |                 4 | Av. Brigadeiro Luís Antônio, 306 - Bela Vista, São Paulo - SP |         4 |                    0 | 2022-04-26 | 13:00:00 |
-| Stop & Park     | 57410487000106 |                 5 | R. da Consolação, 247 - Consolação, São Paulo - SP            |         5 |                    1 | 2022-04-26 | 12:00:00 |
-| Stop & Park     | 57410487000106 |                 5 | R. da Consolação, 247 - Consolação, São Paulo - SP            |         5 |                    0 | 2022-04-26 | 13:00:00 |
-| Stop & Park     | 57410487000106 |                 6 | R. Augusta, 1808 - Cerqueira César, São Paulo - SP            |         6 |                    1 | 2022-04-26 | 12:00:00 |
-| Stop & Park     | 57410487000106 |                 6 | R. Augusta, 1808 - Cerqueira César, São Paulo - SP            |         6 |                    0 | 2022-04-26 | 13:00:00 |
-+-----------------+----------------+-------------------+---------------------------------------------------------------+-----------+----------------------+------------+----------+
+//
 
 
-SELECT Empresa.nome AS "Nome da Empresa", Empresa.cnpj AS CNPJ, CONCAT(Administrador.nome, " ", Administrador.sobrenome) AS Administrador,
+//SELECT Empresa.nome AS "Nome da Empresa", Empresa.cnpj AS CNPJ, CONCAT(Administrador.nome, " ", Administrador.sobrenome) AS Administrador,
 CONCAT(AdmChefe.nome," ", AdmChefe.sobrenome) AS "Administrador Chefe", Estacionamento.idEstacionamento AS "Nº Estacionamento",
 Estacionamento.endereco AS Endereço, Sensor.idSensor AS "Nº Sensor", Dados.state AS "Entra = 1 /Saida = 0", Dados.data AS Data, Dados.hora AS Hora  
 FROM Dados JOIN Empresa ON fkEmpresa = idEmpresa JOIN Estacionamento ON fkEstacionamento = idEstacionamento JOIN Sensor ON fkSensor = idSensor 
 JOIN Administrador ON Administrador.fkEmpresa = Empresa.idEmpresa JOIN Administrador as AdmChefe ON Administrador.fkAdmChefe = AdmChefe.idAdministrador;
 
-+-----------------+----------------+----------------+---------------------+-------------------+---------------------------------------------------------------+-----------+----------------------+------------+----------+
-| Nome da Empresa | CNPJ           | Administrador  | Administrador Chefe | Nº Estacionamento | Endereço                                                      | Nº Sensor | Entra = 1 /Saida = 0 | Data       | Hora     |
-+-----------------+----------------+----------------+---------------------+-------------------+---------------------------------------------------------------+-----------+----------------------+------------+----------+
-| Estapar         | 72423437000149 | Robert Pereira | Karen Albuquerque   |                 2 | Av. São Luís, 112 - Republica, São Paulo - SP                 |         2 |                    0 | 2022-04-26 | 13:00:00 |
-| Estapar         | 72423437000149 | Robert Pereira | Karen Albuquerque   |                 2 | Av. São Luís, 112 - Republica, São Paulo - SP                 |         2 |                    1 | 2022-04-26 | 12:00:00 |
-| Estapar         | 72423437000149 | Robert Pereira | Karen Albuquerque   |                 1 | Av. Paulista, 1374 - Bela Vista, São Paulo - SP               |         1 |                    0 | 2022-04-26 | 13:00:00 |
-| Estapar         | 72423437000149 | Robert Pereira | Karen Albuquerque   |                 1 | Av. Paulista, 1374 - Bela Vista, São Paulo - SP               |         1 |                    1 | 2022-04-26 | 12:00:00 |
-| MultiPark       | 31985344000120 | Monica Costa   | Miguel Ferrari      |                 4 | Av. Brigadeiro Luís Antônio, 306 - Bela Vista, São Paulo - SP |         4 |                    0 | 2022-04-26 | 13:00:00 |
-| MultiPark       | 31985344000120 | Monica Costa   | Miguel Ferrari      |                 4 | Av. Brigadeiro Luís Antônio, 306 - Bela Vista, São Paulo - SP |         4 |                    1 | 2022-04-26 | 12:00:00 |
-| MultiPark       | 31985344000120 | Monica Costa   | Miguel Ferrari      |                 3 | R. Itapeva, 636 - Bela Vista, São Paulo - SP                  |         3 |                    0 | 2022-04-26 | 13:00:00 |
-| MultiPark       | 31985344000120 | Monica Costa   | Miguel Ferrari      |                 3 | R. Itapeva, 636 - Bela Vista, São Paulo - SP                  |         3 |                    1 | 2022-04-26 | 12:00:00 |
-| Stop & Park     | 57410487000106 | Pedro Gomes    | Alice Nakamura      |                 6 | R. Augusta, 1808 - Cerqueira César, São Paulo - SP            |         6 |                    0 | 2022-04-26 | 13:00:00 |
-| Stop & Park     | 57410487000106 | Pedro Gomes    | Alice Nakamura      |                 6 | R. Augusta, 1808 - Cerqueira César, São Paulo - SP            |         6 |                    1 | 2022-04-26 | 12:00:00 |
-| Stop & Park     | 57410487000106 | Pedro Gomes    | Alice Nakamura      |                 5 | R. da Consolação, 247 - Consolação, São Paulo - SP            |         5 |                    0 | 2022-04-26 | 13:00:00 |
-| Stop & Park     | 57410487000106 | Pedro Gomes    | Alice Nakamura      |                 5 | R. da Consolação, 247 - Consolação, São Paulo - SP            |         5 |                    1 | 2022-04-26 | 12:00:00 |
-+-----------------+----------------+----------------+---------------------+-------------------+---------------------------------------------------------------+-----------+----------------------+------------+----------+;
+//
 
