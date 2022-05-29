6926b51cbf1fd0487a43790e387685f59985fbd0
Leonardo-LRD <leonardo.vasconcellos@sptech.school>
2022-05-29 18:10:15 -0300

    configurando banco

--- site/src/database/script-tabelas.sql
+++ site/src/database/script-tabelas.sql
@@ -2,27 +2,28 @@
 -- Você precisa executar os comandos no banco de dados para criar as tabelas,
 -- ter este arquivo aqui não significa que a tabela em seu BD estará como abaixo!
 
-/* para workbench - local - desenvolvimento */
-CREATE DATABASE MAT;
-USE MAT;
+/* para MySQL - local - Desenvolvimento */
 
-CREATE TABLE Empresa (
+CREATE DATABASE mat;
+USE mat;
+
+CREATE TABLE empresa (
 	idEmpresa INT PRIMARY KEY AUTO_INCREMENT,
 	nome VARCHAR(45),
 	cnpj VARCHAR(20)
 );
 
-CREATE TABLE Estacionamento (
+CREATE TABLE estacionamento (
 	idEstacionamento INT PRIMARY KEY AUTO_INCREMENT,
 	cep INT,
 	numero INT,
 	uf VARCHAR(2),	
 	numVagas INT,
 	fkEmpresa INT,
-	FOREIGN KEY (fkEmpresa) REFERENCES Empresa (idEmpresa)
+	FOREIGN KEY (fkEmpresa) REFERENCES empresa (idEmpresa)
 );
 
-CREATE TABLE Usuario (
+CREATE TABLE usuario (
 	idUsuario INT PRIMARY KEY AUTO_INCREMENT,
 	nome VARCHAR(45),
 	sobrenome VARCHAR(45),
@@ -30,29 +31,27 @@ CREATE TABLE Usuario (
 	senha VARCHAR(45),
 	fkEmpresa INT,
 	fkAdmChefe INT,
-	FOREIGN KEY (fkEmpresa) REFERENCES Empresa (idEmpresa),
-	FOREIGN KEY (fkAdmChefe) REFERENCES Usuario (idUsuario)
+	FOREIGN KEY (fkEmpresa) REFERENCES empresa (idEmpresa),
+	FOREIGN KEY (fkAdmChefe) REFERENCES usuario (idUsuario)
 );
 
-CREATE TABLE Usuario_Estacionamento (
+CREATE TABLE usuario_estacionamento (
 	fkUsuario INT,
 	fkEstacionamento INT,
 	PRIMARY KEY (fkUsuario, fkEstacionamento),
-	FOREIGN KEY (fkUsuario) REFERENCES Usuario (idUsuario),
-	FOREIGN KEY (fkEstacionamento) REFERENCES Estacionamento (idEstacionamento)
+	FOREIGN KEY (fkUsuario) REFERENCES usuario (idUsuario),
+	FOREIGN KEY (fkEstacionamento) REFERENCES estacionamento (idEstacionamento)
 );
 
-INSERT INTO Dados(data, hora, state, fkSensor, fkEstacionamento, fkEmpresa,) VALUES (curdate(), curtime(), ?, 1, 1, 1)
-
-CREATE TABLE Sensor (
+CREATE TABLE sensor (
 	idSensor  INT PRIMARY KEY AUTO_INCREMENT,
 	fkEstacionamento INT,
 	fkEmpresa INT,
-	FOREIGN KEY (fkEstacionamento) REFERENCES Estacionamento (idEstacionamento),
-	FOREIGN KEY (fkEmpresa) REFERENCES Empresa (idEmpresa)
+	FOREIGN KEY (fkEstacionamento) REFERENCES estacionamento (idEstacionamento),
+	FOREIGN KEY (fkEmpresa) REFERENCES empresa (idEmpresa)
 );
 
-CREATE TABLE Dados (
+CREATE TABLE dados (
 	idDados INT AUTO_INCREMENT,
 	data DATE,
 	hora TIME,
@@ -61,14 +60,57 @@ CREATE TABLE Dados (
 	fkEstacionamento INT,
 	fkEmpresa INT,
 	PRIMARY KEY (idDados, fkSensor, fkEstacionamento, fkEmpresa),
-	FOREIGN KEY (fkSensor) REFERENCES Sensor (idSensor),
-	FOREIGN KEY (fkEstacionamento) REFERENCES Estacionamento (idEstacionamento),
-	FOREIGN KEY (fkEmpresa) REFERENCES Empresa (idEmpresa)
+	FOREIGN KEY (fkSensor) REFERENCES sensor (idSensor),
+	FOREIGN KEY (fkEstacionamento) REFERENCES estacionamento (idEstacionamento),
+	FOREIGN KEY (fkEmpresa) REFERENCES empresa (idEmpresa)
 );
 
+-- CREATE DATABASE acquatec;
+/*
+USE acquatec;
 
-/* para sql server - remoto - produção */
+CREATE TABLE usuario (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	nome VARCHAR(50),
+	email VARCHAR(50),
+	senha VARCHAR(50)
+);
 
+CREATE TABLE aviso (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	titulo VARCHAR(100),
+	descricao VARCHAR(150),
+	fk_usuario INT,
+	FOREIGN KEY (fk_usuario) REFERENCES usuario(id)
+);
+
+create table aquario (
+-- em nossa regra de negócio, um aquario tem apenas um sensor 
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	descricao VARCHAR(300)
+);
+
+-- altere esta tabela de acordo com o que está em INSERT de sua API do arduino
+
+create table medida (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	dht11_umidade DECIMAL,
+	dht11_temperatura DECIMAL,
+	luminosidade DECIMAL,
+	lm35_temperatura DECIMAL,
+	chave TINYINT,
+	momento DATETIME,
+	fk_aquario INT,
+	FOREIGN KEY (fk_aquario) REFERENCES aquario(id)
+);
+*/
+
+/* para SQL Server - remoto - Produção */
+
+nosso DATABASE Azure aqui;
+
+-- CREATE DATABASE Azure acquatec;
+/*
 CREATE TABLE usuario (
 	id INT PRIMARY KEY IDENTITY(1,1),
 	nome VARCHAR(50),
@@ -79,16 +121,26 @@ CREATE TABLE usuario (
 CREATE TABLE aviso (
 	id INT PRIMARY KEY IDENTITY(1,1),
 	titulo VARCHAR(100),
-    descricao VARCHAR(150),
+	descricao VARCHAR(150),
 	fk_usuario INT FOREIGN KEY REFERENCES usuario(id)
-); 
+);
+
+create table aquario (
+-- em nossa regra de negócio, um aquario tem apenas um sensor 
+	id INT PRIMARY KEY IDENTITY(1,1),
+	descricao VARCHAR(300)
+);
+
+-- altere esta tabela de acordo com o que está em INSERT de sua API do arduino 
 
 CREATE TABLE medida (
 	id INT PRIMARY KEY IDENTITY(1,1),
-	temperatura DECIMAL,
-	umidade DECIMAL,
+	dht11_umidade DECIMAL,
+	dht11_temperatura DECIMAL,
+	luminosidade DECIMAL,
+	lm35_temperatura DECIMAL,
+	chave TINYINT,
 	momento DATETIME,
-	fk_aquario INT
+	fk_aquario INT FOREIGN KEY REFERENCES aquario(id)
 );
-
-
+*/
